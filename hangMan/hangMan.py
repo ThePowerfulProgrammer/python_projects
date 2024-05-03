49fdc10b3a843d22c6c3c8e59120bf09c70c2cd9
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-03 22:06:31 +0200

    now make it better

--- hangMan/hangMan.py
+++ hangMan/hangMan.py
@@ -1,23 +1,65 @@
 import random
 
-randomWord = ""
-with open("commonWords.txt") as rFile:
-    words = rFile.readlines()
-    randomWord = random.choice(words)
+# randomWord = ""
+# with open("commonWords.txt") as rFile:
+#     words = rFile.readlines()
+#     randomWord = random.choice(words)
     
-print(f"The mystery word is {randomWord}")
+# print(f"The mystery word is {randomWord}")
 
-hiddenWord = []
-for _ in range(len(randomWord) - 1):
-    hiddenWord += "_"
+# hiddenWord = []
+# for _ in range(len(randomWord) - 1):
+#     hiddenWord += "_"
 
-print(f"{'_'*(len(randomWord) - 1) }")
+# print(f"{'_'*(len(randomWord) - 1) }")
     
-guess = input("Guess a character in the mystery word: ").lower()
+# guess = input("Guess a character in the mystery word: ").lower()
 
-for pos in range(len(randomWord) - 1):
-    if randomWord[pos] == guess:
-        hiddenWord[pos] = guess
+# for pos in range(len(randomWord) - 1):
+#     if randomWord[pos] == guess:
+#         hiddenWord[pos] = guess
 
-print(hiddenWord) 
+# print(hiddenWord) 
     
+    
+def generateHiddenWord():
+    randomWord = ""
+    with open("commonWords.txt") as rFile:
+        words = rFile.readlines()
+        randomWord = random.choice(words)
+        
+    return randomWord
+
+def beginGame(randomWord: str):
+    print("Hello, let's play hangman :^) ")
+    print(randomWord)
+    lenRandomWord = len(randomWord)-1
+    userLives = random.randint(lenRandomWord, lenRandomWord+3)
+    
+    print(f"The hidden word is {'_'*lenRandomWord}")    
+    
+    hiddenWord = []
+    for _ in range(lenRandomWord):
+        hiddenWord += '_'
+        
+    
+    
+    while (userLives > 0 and '_' in hiddenWord):
+        guess = input("Take a guess: ")
+                
+        if guess in randomWord:
+            for pos in range(lenRandomWord):
+                if randomWord[pos] == guess:
+                    hiddenWord[pos] = guess
+            print(''.join(hiddenWord))
+        else:
+            print("Wrong guess, try again")
+            userLives -= 1
+            print(''.join(hiddenWord))
+            
+    print(''.join(hiddenWord))
+            
+    
+    return None
+
+print(beginGame(generateHiddenWord()))    
