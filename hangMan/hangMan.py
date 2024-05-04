9b5c85eeb606f8ffd7158cd6d9b0f3a34412c45f
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-04 16:15:29 +0200

    Fixed && understoof

--- hangMan/hangMan.py
+++ hangMan/hangMan.py
@@ -1,54 +1,39 @@
 import random
 
-# randomWord = ""
-# with open("commonWords.txt") as rFile:
-#     words = rFile.readlines()
-#     randomWord = random.choice(words)
-    
-# print(f"The mystery word is {randomWord}")
-
-# hiddenWord = []
-# for _ in range(len(randomWord) - 1):
-#     hiddenWord += "_"
-
-# print(f"{'_'*(len(randomWord) - 1) }")
-    
-# guess = input("Guess a character in the mystery word: ").lower()
-
-# for pos in range(len(randomWord) - 1):
-#     if randomWord[pos] == guess:
-#         hiddenWord[pos] = guess
-
-# print(hiddenWord) 
-    
-    
+ 
+# choose a random word from a txt file
 def generateHiddenWord():
     randomWord = ""
     with open("commonWords.txt") as rFile:
         words = rFile.readlines()
-        randomWord = random.choice(words)
+        randomWord = random.choice(words).strip()
         
     return randomWord
 
 def beginGame(randomWord: str):
-    print("Hello, let's play hangman :^) ")
-    print(randomWord)
-    lenRandomWord = len(randomWord)-1
-    userLives = random.randint(lenRandomWord, lenRandomWord+3)
+    showWord = int(input("Select 1 to display partial word and 0 to play on hard word: "))
+    if (showWord == 1):
+        print("".join(random.sample(randomWord, 2)))
+    
+    
+    lenRandomWord = len(randomWord) # for later use grab the length of word
+    userLives = random.randint(lenRandomWord, lenRandomWord+3) # worse cause user needs as many chances to guess as there are characters in word
     
     print(f"The hidden word is {'_'*lenRandomWord}")    
+    print(f"You have {userLives} guesses remaining")
     
-    hiddenWord = []
-    for _ in range(lenRandomWord):
-        hiddenWord += '_'
+    # populate list with _ as word so far is unguessed
+    hiddenWord = ['_' for _ in range(lenRandomWord)]
         
+    # while user has guesses and the presence of _ indicates that the word has not been guessed completely 
     while (userLives > 0 and '_' in hiddenWord):
         guess = input("Take a guess: ")
                 
         if guess in randomWord:
             for pos in range(lenRandomWord):
                 if randomWord[pos] == guess:
-                    hiddenWord[pos] = guess
+                    hiddenWord[pos] = guess # update hidden word to reflect correct user guess
+                    
             print(''.join(hiddenWord))
         else:
             print("Wrong guess, try again")
@@ -57,10 +42,11 @@ def beginGame(randomWord: str):
             
     if (userLives == 0):
         print("You lost!!! ")
+        print(f"The word was {randomWord}")
         return "Lost."
     else:
         print(f"Congrats, the hidden word was {''.join(hiddenWord)}")
         return "Win!!!"        
 
-
+print("Hello, let's play hangman :^) ")
 print(beginGame(generateHiddenWord()))    
